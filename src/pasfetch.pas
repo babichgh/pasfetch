47bd33f84bdf43f0e7dfbf53cf49a78a574ace0e
babichgh <ivan20.babich@gmail.com>
2026-05-02 05:56:18 +0300

    Make procedure to OS information printing

--- src/pasfetch.pas
+++ src/pasfetch.pas
@@ -1,26 +1,29 @@
 program pasfetch;
 
-{$mode objfpc}
-
 uses
 	Classes;
+
+procedure PrintOSInfo;
 var
 	Config: TStringList;
-	OSName, OSVersion: string;
+        OSName, OSVersion: string;
 begin
-	Config := TStringList.Create;
+        Config := TStringList.Create;
+ 
+        
+        Config.LoadFromFile('/etc/os-release');
 
-	try
-		Config.LoadFromFile('/etc/os-release');
+        OSName := Config.Values['NAME'];
+        OSVersion := Config.Values['VERSION'];
 
-		OSName := Config.Values['NAME'];
-		OSVersion := Config.Values['VERSION'];
+        OSName := Copy(OSName, 2, Length(OSName) - 2);
+        OSVersion := Copy(OSVersion, 2, Length(OSVersion) - 2);
 
-		OSName := Copy(OSName, 2, Length(OSName) - 2);
-		OSVersion := Copy(OSVersion, 2, Length(OSVersion) - 2);
-	finally
-		Config.Free;
-	end;
+        Config.Free;
 
-	writeln('OS: ', OSName, ' ', OSVersion)
+        writeln('OS: ', OSName, ' ', OSVersion)
+end;
+
+begin
+	PrintOSInfo
 end.
